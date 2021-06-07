8988e4c346dbc8aaaa24ce23e1ad3c357eff8ff1
Shabbeer Hassan <44751331+Shabs86@users.noreply.github.com>
2021-06-07 15:51:01 +0300

    Update and rename rsid2bed.sh.rtf to rsid2bed.sh

--- rsid2bed.sh
+++ rsid2bed.sh
@@ -7,16 +7,15 @@ SNPS=$(pwd)/$1
 echo Proccesing file:
 echo $SNPS 
 
-#check if working folder exist, if not, create
-
+#check if working folder exists, if not, then create
 if [ ! -d $DIR ]
 then
 mkdir ~/rsID2Bed
 fi
 
 cd ~/rsID2Bed
-#check if dbsnp file exists, if not, download from snp147Common table using mysql
 
+#check if dbsnp file exists, if not, download from snp147Common table using mysql
 if [ ! -f $FILE ]
 then
 mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -N -D hg19 -e 'SELECT chrom, chromStart, chromEnd, name FROM snp147Common' > snp147Common.bed
